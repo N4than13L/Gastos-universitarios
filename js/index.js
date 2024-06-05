6a7bfea40805e26b62a7770d6156e69f27242164
Jose Nathaniel Bonilla <natharevolution@gmail.com>
2024-06-05 16:57:45 -0400

    haciendo cambios al codigo

--- js/index.js
+++ js/index.js
@@ -1,20 +1,19 @@
+function calcular_costos(actividad, dias, resultado) {
+  let id = document.getElementById(resultado);
+  return (id.innerText = "Total: $ " + actividad * dias);
+}
+
+function total_gastos(pasajes, comida, folletos, resultado) {
+  let id = document.getElementById(resultado);
+  return (id.innerText =
+    "Total de gastos: $ " +
+    parseInt(pasajes, 0) +
+    parseInt(comida, 0) +
+    folletos);
+}
+
 // calcular semestre
 var calcular = document.getElementById("calcular");
-
-function calcular_pasaje(pasaje, dias, resultado) {
-  let id = document.getElementById(resultado);
-  return (id.innerText = "Total: $ " + pasaje * dias);
-}
-
-function calcular_comida(comida, dias, resultado) {
-  let id = document.getElementById(resultado);
-  return (id.innerText = "Total: $ " + comida * dias);
-}
-
-function calcular_folletos(folletos, dias, resultado) {
-  let id = document.getElementById(resultado);
-  return (id.innerText = "Total: $ " + folletos * dias);
-}
 
 // funcion para calcular los gastos mendiante el click del mouse.
 calcular.addEventListener("click", (e) => {
@@ -38,7 +37,7 @@ calcular.addEventListener("click", (e) => {
     "total_pasaje_diario"
   ).innerHTML = gastos_pasaje_diario);
 
-  calcular_pasaje(gastos_pasaje, gastos_pasaje_diario, "resultado_del_pasaje");
+  calcular_costos(gastos_pasaje, gastos_pasaje_diario, "resultado_del_pasaje");
 
   // gastos comida.
   var gasto_comida = document.getElementById("gasto_comida").value;
@@ -49,7 +48,7 @@ calcular.addEventListener("click", (e) => {
     "total_gastos_comida"
   ).innerHTML = gastos_comida_dia);
 
-  calcular_comida(gasto_comida, gastos_comida_dia, "resultado_total_comida");
+  calcular_costos(gasto_comida, gastos_comida_dia, "resultado_total_comida");
 
   // folletos.
   var gasto_folleto = document.getElementById("gasto_folleto").value;
@@ -64,17 +63,20 @@ calcular.addEventListener("click", (e) => {
     "total_a_pagar_folletos"
   ).innerHTML = gastos_diarios_folletos);
 
-  calcular_folletos(
-    gasto_folleto,
-    gastos_diarios_folletos,
-    "resultado_folleto"
-  );
+  calcular_costos(gasto_folleto, gastos_diarios_folletos, "resultado_folleto");
 
   // pago del semestre.
   var gasto_semestre = document.getElementById("gasto_semestre").value;
   var pago_semestre = (document.getElementById("pago_semestre").innerHTML =
     gasto_semestre);
 
-  var total_semestre = (document.getElementById("total_semestre").innerHTML =
+  var gastos_diarios = (document.getElementById("gastos_diarios").innerHTML =
     gasto_semestre);
+
+  total_gastos(
+    "resultado_del_pasaje",
+    "resultado_total_comida",
+    "resultado_folleto",
+    "total_a_gastar"
+  );
 });
